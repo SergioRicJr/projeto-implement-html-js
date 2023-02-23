933fcff7c407899a7b4d469b30f212336e5b0c04
Sergio <sergioricjr7@gmail.com>
2023-02-23 20:50:02 -0300

    funcionando, fazer limpeza de codigo e deixar mais bonito

--- src/pagina-teste.js
+++ src/pagina-teste.js
@@ -1,19 +1,16 @@
 const body = document.querySelector("body")
+const main = document.querySelector("main")
 
 var contentHTML = JSON.parse(localStorage.getItem("texto-html")).content
 var contentJS = JSON.parse(localStorage.getItem("texto-javascript")).content
-console.log(contentHTML)
-console.log(contentJS)
 var divHtml = document.createElement("div")
 divHtml.innerHTML = contentHTML
 var tagScript = document.createElement("script")
 var texto = document.createTextNode(contentJS)
 tagScript.appendChild(texto)
-// tagScript.innerHTML = contentJS
 console.log(tagScript)
-body.appendChild(divHtml)
+main.appendChild(divHtml)
 body.appendChild(tagScript)
-// localStorage.clear()
 
 /* <h1>oiieee</h1>
 <button id="btn">Entrar</button>
